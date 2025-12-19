537321be00d5d79c7840aad1d4aac22af971cebe
Luiz <luizcunha3@gmail.com>
2025-12-19 17:33:28 -0300

    add info function

--- install.sh
+++ install.sh
@@ -1,5 +1,12 @@
-echo "Instalando git"
-sudo pacman -S git
+#!/bin/bash
 
-echo "Instalando zsh" 
-sudo pacman -S zsh
+# Função para mensagens coloridas
+info() {
+    echo -e "\e[34m[INFO]\e[0m $1"
+}
+
+info "Instalando git"
+sudo pacman -S --noconfirm --needed git
+
+info "Instalando zsh"
+sudo pacman -S --noconfirm --needed zsh
