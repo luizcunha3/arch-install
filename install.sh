90b823f1b57131557f41eb7dd7e4c890e6f057b8
Luiz <luizcunha3@gmail.com>
2025-12-19 17:41:25 -0300

    adds oh my zsh

--- install.sh
+++ install.sh
@@ -10,3 +10,9 @@ sudo pacman -S --noconfirm --needed git
 
 info "Instalando zsh"
 sudo pacman -S --noconfirm --needed zsh
+
+info "Definindo zsh como shell padrao"
+chsh -s $(which zsh)
+
+info "Instalando Oh My ZSH" 
+sh -c "$(wget https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh -O -)"
